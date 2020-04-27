0a95d16f8177cc3570df4a3f885b33b71f3f851a
Sanne Peters <sanne@sanstream.nl>
2020-04-27 10:13:59 +0200

    input via command line options, outputs a ndjson file

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const fs = require('fs')
 const yaml = require('yaml')
 const ndjson = require('ndjson')
+const chalk = require('chalk')
+
+const [ouputFileName, inputDirectory ] = Array.from(process.argv).reverse()
 
 const serialize = ndjson.serialize()
 
 serialize.on('data', function(line) {
   // line is a line of stringified JSON with a newline delimiter at the end
-  fs.writeFileSync('out.ndjson', line, { flag: 'a' }, err => {})
+  fs.writeFileSync(ouputFileName, line, { flag: 'a' }, err => {})
 })
-// const testHTML = '/home/sanne/Projects/Code/sanstream-jekyll/front-end/_posts/2013-08-11-a-conversion-diagram.html'
-// const testMD = '/home/sanne/Projects/Code/sanstream-jekyll/front-end/_posts/ 2015-08-23-charting-emotions.md'
-const dir = '/home/sanne/Projects/Code/sanstream-jekyll/front-end/_posts/'
-fs.readdirSync(dir, 'utf-8').forEach(fileName => {
-  const file = fs.readFileSync(dir + fileName,  'utf8')
+console.log(`Parsing directory...`)
+fs.readdirSync(inputDirectory, 'utf-8').forEach(fileName => {
+  const file = fs.readFileSync(inputDirectory + fileName,  'utf8')
+  // deal with the yaml frontmatter and content separately
   const fileChunks = file.split('---\n')
-  const yamlFrontmatter = yaml.parse(fileChunks[1])
-  const content = fileChunks[2]
-  console.log(fileChunks.length)
-  
-  serialize.write({
-    ...yamlFrontmatter,
-    content,
-  })
+  if (fileChunks.length === 3) {
+    const yamlFrontmatter = yaml.parse(fileChunks[1])
+    const content = fileChunks[2]
+
+    serialize.write({
+      ...yamlFrontmatter,
+      content,
+    })
+  } else {
+    console.log(chalk.red(`There is something off with '${fileName}'`))
+  }
 })
 serialize.end()
+
+console.log(chalk.green(`Output written to '${ouputFileName}'`))
 
